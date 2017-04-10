8ed57e8701a28113fa8009efb13c2c2c0328f21b
pauwel.rummens@cloudar.be <pauwel.rummens@cloudar.be>
2017-04-10 13:29:53 +0200

    changes to the zip process

--- scripts/reporting-rshiny-waterbirds.sh
+++ scripts/reporting-rshiny-waterbirds.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu/reporting-rshiny-waterbirds
 if [ -f reporting-rshiny-waterbirds.tar.gz ]; then
     rm reporting-rshiny-waterbirds.tar.gz
 fi
-tar -zcvf reporting-rshiny-waterbirds.tar.gz reporting-rshiny-waterbirds
+tar -zcvf reporting-rshiny-waterbirds.tar.gz watervogelsAnalyse
 # Build the docker image
 sudo docker build -t openanalytics/watervogels .
